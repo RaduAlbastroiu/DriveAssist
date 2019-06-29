89f39d965f22592a35acec15a581448bf7ce05f5
RaduAlbastroiu <albastroiuradug@gmail.com>
2019-06-29 22:11:18 +0300

    Computed speed

--- DriveAssist/DriveAssist/MapController.swift
+++ DriveAssist/DriveAssist/MapController.swift
@@ -13,8 +13,12 @@ class MapController: NSObject, MKMapViewDelegate {
     
     private var mkMapView: MKMapView
     private var shouldCenterMapOnLocation: Bool
+    
+    private var previousLocations: [CLLocation]
+    
     var currentLocation: CLLocation?
     var showIssues: Bool
+    var currentSpeed = 0.0
     var viewController: ViewController
     
     init(mapView: MKMapView, viewController: ViewController) {
@@ -26,6 +30,8 @@ class MapController: NSObject, MKMapViewDelegate {
         mapView.showsCompass = true
         mapView.showsScale = true
         self.viewController = viewController
+        
+        self.previousLocations = []
         
         super.init()
         mapView.delegate = self
@@ -43,6 +49,27 @@ class MapController: NSObject, MKMapViewDelegate {
         let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: radius, longitudinalMeters: radius)
         mkMapView.setRegion(coordinateRegion, animated: true)
     }
+    
+    private func computeSpeed() {
+        if(previousLocations.count > 10) {
+            var newPreviousLocations = [CLLocation]()
+            for index in previousLocations.count - 4 ... previousLocations.count {
+                newPreviousLocations.append(previousLocations[index])
+            }
+            
+            previousLocations = newPreviousLocations
+        }
+        
+        if( previousLocations.count > 3) {
+            var totalSpeed = 0.0
+            
+            for index in previousLocations.count...previousLocations.count - 2 {
+                totalSpeed += previousLocations[index].speed
+            }
+            
+            currentSpeed = totalSpeed / 3.0
+        }
+    }
 }
 
 extension MapController: LocationManagerDelegate {
@@ -51,6 +78,7 @@ extension MapController: LocationManagerDelegate {
             if(locations.count > 0) {
                 centerMapOn(location: locations[0], withRadius: 1000)
                 shouldCenterMapOnLocation = false
+                previousLocations.append(locations[0])
             }
         }
         currentLocation = locations[0]
